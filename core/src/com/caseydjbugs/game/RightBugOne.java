35dfd63aaf33e1ed9c3b2acc3b81aac4a9b0b2eb
David Casey <caseydj@dukes.jmu.edu>
2016-01-19 16:21:17 -0500

    second

--- core/src/com/caseydjbugs/game/RightBugOne.java
+++ core/src/com/caseydjbugs/game/RightBugOne.java
@@ -15,7 +15,7 @@ public class RightBugOne extends Bug {
     RightBugOne() {
         bounds = new Rectangle();
 
-        this.setX(Gdx.graphics.getHeight()+400);
+        this.setX(Gdx.graphics.getHeight()+450);
         this.setY(Gdx.graphics.getWidth()-50);
         atlas_RightBugOne_run = new TextureAtlas(Gdx.files.internal("RightBugOne.atlas"));
         run = new Animation(1 / 15f, atlas_RightBugOne_run.getRegions());
